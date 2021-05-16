25f55200e1114a55ebe9b48026e61f1b5213e13e
Boshra Jaber <boshrajaber94@gmail.com>
2021-05-17 02:41:17 +0300

    init

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ToDo from './components/todo/todo.js';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <ToDo />
+//       </>
+//     );
+//   }
+// }
+
+function App () {
+    return (
+      <>
+        <ToDo />
+      </>
+    );
+}
+
+export default App;
